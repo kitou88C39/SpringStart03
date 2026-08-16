156ea63817085ea169719e20f5c28e6c2d67dd50
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-16 15:49:09 +0900

    SpringStart05を作成

--- src/main/java/SpringStart/SpringStart05/com.example.demo.controller/WelcomMvcController.java
+++ src/main/java/SpringStart/SpringStart05/com.example.demo.controller/WelcomMvcController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomMvcontroller {
