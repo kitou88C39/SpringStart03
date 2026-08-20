c36cca7a699d765664b61240401084b9edf5519e
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-20 16:55:47 +0900

    SpringStart10を作成

---
+++ src/main/java/SpringStart/SpringStart10/com.example.demo/controller/ReviewController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.ReviewRegistForm;
+
+@Controller
+public class ReviewController {
+
+    @GetMapping("/show-review-form")
+    public String showReviewForm(@ModelAttribute ReviewRegistForm form){
+        return "regist-review";
+    }
+
+    @PostMapping("/show-review-form-ret")
+    public String registReviewFormRet(@ModelAttribute RegistReviewForm form){
+        return "regist-review";
+    }
+
+    @PostMapping("/show-review")
+    public String registReview(@ModelAttribute RegistReviewForm form){
+        return "confirm-regist-review";
+    }
+
+    @PostMapping("/confirm-regist-review")
+    public String confirmRegistReview(RegistReviewForm form, Model model){
+
+    model.addAttribute("msg","レビュー登録が完了しました。");d
+
+    return "complete-regist-review";
+    }
+}
