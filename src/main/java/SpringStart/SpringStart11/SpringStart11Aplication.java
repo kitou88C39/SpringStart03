ec13c24fb1f32154fbf415ebf45691dec1219b7b
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-22 21:06:06 +0900

    SpringStart11を作成

---
+++ src/main/java/SpringStart/SpringStart11/SpringStart11Aplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class SpringStart11Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringStart11Application.class, args);
+    }
+
+}
