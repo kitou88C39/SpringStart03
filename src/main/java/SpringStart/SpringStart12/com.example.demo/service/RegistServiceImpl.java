cf8939f619227eaad0f1f61ea248c51dfcfd7ed3
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-24 11:12:44 +0900

    SpringStart12を作成

--- src/main/java/SpringStart/SpringStart12/com.example.demo/service/RegistServiceImpl.java
+++ src/main/java/SpringStart/SpringStart12/com.example.demo/service/RegistServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Review;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class RegistServiceImpl implements RegistService {
 
     private final RegistRepository repository;
