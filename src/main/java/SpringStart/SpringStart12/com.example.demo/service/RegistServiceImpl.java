d6694e33fdb422468ff5c0ad98880f634944a4c0
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-24 10:53:42 +0900

    SpringStart12を作成

---
+++ src/main/java/SpringStart/SpringStart12/com.example.demo/service/RegistServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Review;
+
+public class RegistServiceImpl implements RegistService {
+
+    @Override
+    public void regist(Review review){
+    }
+}
