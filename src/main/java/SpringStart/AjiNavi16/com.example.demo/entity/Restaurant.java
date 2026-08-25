941b826852686156b0cd7ba7c1ab331b792c690f
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-25 20:58:31 +0900

    AjiNavi16を作成

--- src/main/java/SpringStart/AjiNavi16/com.example.demo/entity/Restaurant.java
+++ src/main/java/SpringStart/AjiNavi16/com.example.demo/entity/Restaurant.java
@@ -1,5 +1,12 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Restaurant {
 
     private int restaurantId;
