1fe879d3824e21bd333c0f43c1442e3f826ee19f
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-25 20:53:54 +0900

    AjiNavi16を作成

--- src/main/java/SpringStart/AjiNavi16/com.example.demo/entity/Restaurant.java
+++ src/main/java/SpringStart/AjiNavi16/com.example.demo/entity/Restaurant.java
@@ -2,5 +2,20 @@ package com.example.demo.entity;
 
 public class Restaurant {
 
+    private int restaurantId;
+    private String restaurantName;
+    private String catchPhrase;
+    private double avarageRating;
 
+    //平均評価表示
+    public String formatAverageRating() {
+        String result;
+
+        if(avarageRating == 0.0) {
+            result = "-";
+        } else {
+            result = String.format("% 1f", avarageRating);
+        }
+        return result;
+    }
 }
