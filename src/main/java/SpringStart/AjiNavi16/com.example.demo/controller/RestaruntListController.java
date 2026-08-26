fe4454a0109f159087d10d6092c5651506f0e4bb
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-26 10:55:31 +0900

    AjiNavi16を作成

--- src/main/java/SpringStart/AjiNavi16/com.example.demo/controller/RestaruntListController.java
+++ src/main/java/SpringStart/AjiNavi16/com.example.demo/controller/RestaruntListController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.example.demo.form.RestaurantSearchForm;
@@ -11,8 +12,23 @@ import com.example.demo.form.RestaurantSearchForm;
 public class RestaurantListController {
 
     @GetMapping("/top")
-    private String RestaurantList(
+    private String restaurantList(
            @ModelAttribute RestaurantSearchForm form) {
+        return "restaurant-list";
+    }
+
+    @PostMapping("/restaurant-search")
+    private String restaurantSearch(
+           @ModelAttribute RestaurantSearchForm form,
+           Model model) {
+
+        List<Restaurant> list = new ArrayList<Restaurant>();
+        list.add(new Restaurant(1,"店舗1","キャッチ1", 3.5));
+        list.add(new Restaurant(2,"店舗2","キャッチ2", 3.6));
+        list.add(new Restaurant(3,"店舗3","キャッチ3", 3.7));
+
+        model.addAttribute("restaurantList", list);
+
         return "restaurant-list";
     }
 }
