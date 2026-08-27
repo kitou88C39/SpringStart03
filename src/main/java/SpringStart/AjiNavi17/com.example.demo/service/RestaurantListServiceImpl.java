032a7a7242a5de29fe7c4e98ae5a07b10ab6f02f
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-27 19:22:38 +0900

    AjiNavi17を作成

--- src/main/java/SpringStart/AjiNavi17/com.example.demo/service/RestaurantListServiceImpl.java
+++ src/main/java/SpringStart/AjiNavi17/com.example.demo/service/RestaurantListServiceImpl.java
@@ -3,14 +3,20 @@ package com.example.demo.service;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import com.example.demo.entity.Restaurant;
+import com.example.demo.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class RestaurantListServiceImple implements RestaurantListService {
 
     private final RestaurantRepository repository;
 
     @Override
     public list<Restaurant> findByNameWildcard(String restaurantName){
-        return null;
+        List<Restaurant> list
+            = repository.selectByNameWildcard(restaurantName);
+
+        return list;
     }
 }
