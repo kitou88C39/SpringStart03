14242b77977f3dd5bc1c68b668bf96448a237dfd
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-28 14:57:15 +0900

    AjiNavi17を作成

--- src/main/java/SpringStart/AjiNavi17/com.example.demo/repository/RestaurantRepositoryImpl.java
+++ src/main/java/SpringStart/AjiNavi17/com.example.demo/repository/RestaurantRepositoryImpl.java
@@ -34,8 +34,33 @@ public class RestaurantRepositoryImpl implements ReviewRepository {
       " mr.restaurant_id,                            "+
       " mr.restaurant_name,                          "+
       " mr.catch_phrase                              "+
-      "ORDER BY"+
+      "ORDER BY                                      "+
       " mr.restaurant_id                             ";
-    return null;
+
+    String p = "%" + restaurantName + "%";
+
+    List<Map<String, Object>> List
+        = jdbcTemplate.queryForList(sql, p);
+
+    //値の取得⇨結果の格納
+
+    List<Restaurant> result = new ArrayList<>();
+        for (Map<String, Object> map : list) {
+            Restaurant restaurant = new Restaurant();
+            restaurant.setRestaurantId(
+                (int) one.get("restaurant_id")
+            );
+            restaurant.setRestaurantName(
+                (String) one.get("restaurant_name")
+            );
+            restaurant.setCatchPhrase(
+                (String) one.get("catch_phrase")
+            );
+            restaurant.setAvarageRating(
+                ((Number) one.get("avarage_rating")).doubleValue()
+            );
+            result.add(restaurant);
+        }
+    return result;
     }
 }
