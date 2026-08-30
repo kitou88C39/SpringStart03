12457f4757078f66cffcc01947f81ee8cc1cee69
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-30 11:51:05 +0900

    AjiNavi20を作成

--- src/main/java/SpringStart/AjiNavi20/com.example.demo/controller/ReviewListController.java
+++ src/main/java/SpringStart/AjiNavi20/com.example.demo/controller/ReviewListController.java
@@ -3,12 +3,14 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.form.ReviewSearchForm;
 
 @Controller
 public class ReviewListController {
 
+    @PostMapping("/search-review")
     private String searchReview(
     @ModelAttribute ReviewSearchForm form,
     Model model) {
