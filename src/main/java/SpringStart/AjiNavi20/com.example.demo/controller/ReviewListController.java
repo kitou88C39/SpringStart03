fb3c1b5e9152821b53e37ffbf864166f5eadd6c3
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-30 11:47:31 +0900

    AjiNavi20を作成

--- src/main/java/SpringStart/AjiNavi20/com.example.demo/controller/ReviewListController.java
+++ src/main/java/SpringStart/AjiNavi20/com.example.demo/controller/ReviewListController.java
@@ -1,8 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.demo.form.ReviewSearchForm;
 
 @Controller
 public class ReviewListController {
 
+    private String searchReview(
+    @ModelAttribute ReviewSearchForm form,
+    Model model) {
+
+    }
 }
