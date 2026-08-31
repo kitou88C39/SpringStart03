5bf3258b31073e704a109442567de629c0acc180
kitoumasaomi <kanfutrooper@gmail.com>
2026-08-31 20:27:33 +0900

    AjiNavi22を作成

--- src/main/java/SpringStart/AjiNavi22/com.example.demo/aop/LoggingAspect.java
+++ src/main/java/SpringStart/AjiNavi22/com.example.demo/aop/LoggingAspect.java
@@ -1,5 +1,10 @@
 package com.example.demo.aop;
 
+import org.springframework.web.bind.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
 
 }
