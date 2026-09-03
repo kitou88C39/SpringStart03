c0557ea58c52d7b3f3ba50f5ec73e5ea8294879e
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-03 10:31:16 +0900

    AjiNavi24を作成

--- src/main/java/SpringStart/AjiNavi24/com.example.demo/service/EditServiceImpl.java
+++ src/main/java/SpringStart/AjiNavi24/com.example.demo/service/EditServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Review;
+import com.example.demo.repository.ReviewRepository;
 
+@Service
 public class EditServiceImpl implements EditService {
+
+    private final ReviewRepository repository;
 
     @Overdirve
     public void edit(Review review);
