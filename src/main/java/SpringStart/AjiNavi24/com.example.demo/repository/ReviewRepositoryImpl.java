e5c08333498fd8095917d6a060b21f8b48251af2
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-03 10:58:33 +0900

    AjiNavi24を作成

--- src/main/java/SpringStart/AjiNavi24/com.example.demo/repository/ReviewRepositoryImpl.java
+++ src/main/java/SpringStart/AjiNavi24/com.example.demo/repository/ReviewRepositoryImpl.java
@@ -17,46 +17,46 @@ import lombok.RequiredArgsConstructor;
 public class ReviewRepositoryImpl implements ReviewRepository {
 
 	private final JdbcTemplate jdbcTemplate;
-
+	
 	@Override
 	public void add(Review review) {
 
 		String sql =
 			" INSERT INTO t_review " +
 			" (restaurant_id, user_id, visit_date, rating, comment) " +
-			" VALUES (?, ?, ?, ?, ?) ";
+			" VALUES (?, ?, ?, ?, ?) ";	
 
 		jdbcTemplate.update(sql, review.getRestaurantId(),
 								 review.getUserId(),
 								 review.getVisitDate(),
 								 review.getRating(),
 								 review.getComment()		);
-
+		
 	}
 
 	@Override
 	public List<Review> selectByRestaurantId(int restaurantId) {
 
-		String sql =
-				"  SELECT                 " +
-				"    review_id,           " +
-				"    restaurant_id,       " +
-				"    user_id,             " +
-				"    visit_date,          " +
-				"    rating,              " +
-				"    comment              " +
-				"  FROM                   " +
-				"    t_review             " +
-				"  WHERE                  " +
-				"    restaurant_id = ?    " +
-				"  ORDER BY               " +
-				"    visit_date DESC,     " +
+		String sql = 
+				"  SELECT                 " + 
+				"    review_id,           " + 
+				"    restaurant_id,       " + 
+				"    user_id,             " + 
+				"    visit_date,          " + 
+				"    rating,              " + 
+				"    comment              " + 
+				"  FROM                   " + 
+				"    t_review             " + 
+				"  WHERE                  " + 
+				"    restaurant_id = ?    " + 
+				"  ORDER BY               " + 
+				"    visit_date DESC,     " + 
 				"    review_id ASC        ";
-
+		
 		// SQLで検索（プレースホルダ：引数で受け取ったrestaurantId）
-		List<Map<String, Object>> list
+		List<Map<String, Object>> list 
 						= jdbcTemplate.queryForList(sql, restaurantId);
-
+		
 		// 値の取得⇒結果の格納
 		List<Review> result = new ArrayList<Review>(); // 結果の初期化
 		for (Map<String, Object> one : list) {
@@ -71,6 +71,29 @@ public class ReviewRepositoryImpl implements ReviewRepository {
 		}
 
 		return result;
+	}
+
+	@Override
+	public void update(Review review) {
+
+		String sql =
+				" UPDATE             " + 
+				"   t_review         " + 
+				" SET                " + 
+				"   user_id = ? ,    " + 
+				"   visit_date = ? , " + 
+				"   rating = ?,      " + 
+				"   comment = ?      " + 
+				" WHERE              " + 
+				"   review_id = ?    ";
+		
+		jdbcTemplate.update(sql, 
+				review.getUserId(),
+				review.getVisitDate(),
+				review.getRating(),
+				review.getComment(),
+				review.getReviewId()  );
+		
 	}
 
 }
